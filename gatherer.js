d9008f97666806580efc8089ed73af29ab2c7049
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-05-15 16:43:54 +0300

    Add cron job to retrieve statistics every hour.

--- gatherer.js
+++ gatherer.js
@@ -1,8 +1,11 @@
 var request = require('request'),
+    CronJob = require('cron').CronJob,
     url = 'http://stats.cyanogenmod.org/api?method=get_counts';
 
-request(url, function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+var job = new CronJob('00 00 * * * *', function() {
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    }
+  });
+}, null, true, null);

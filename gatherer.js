401f464702ae671856d758d090ac7da1f20eddf5
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-05-17 16:15:57 +0300

    Add petsistance to mongodb.

--- gatherer.js
+++ gatherer.js
@@ -1,11 +1,38 @@
 var request = require('request'),
     CronJob = require('cron').CronJob,
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    _ = require('underscore'),
     url = 'http://stats.cyanogenmod.org/api?method=get_counts';
+
+mongoose.connect('mongodb://localhost/cyanogenmod');
+
+var versionSchema = new Schema({
+  timestamp: { type: Date, default: Date.now },
+  statistics: [{ name: String, downloads: Number, _id: false }]
+});
+
+var Version = mongoose.model('Version', versionSchema);
+
+var getVersions = function(rawData) {
+  var data = JSON.parse(rawData);
+  return _.map(data.result.version, function(d) {
+    return {
+      name: d[1],
+      downloads: d[0]
+    };
+  });
+};
 
 var job = new CronJob('00 00 * * * *', function() {
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body);
+      var version = new Version({ statistics: getVersions(body) });
+      version.save(function(err) {
+        if (err) {
+          console.log(err);
+        }
+      });
     }
   });
 }, null, true, null);
